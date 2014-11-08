dffd4a9cd415765280ffe892aa663e27ccc68688
Alvaro Cabrera <pateketrueke@gmail.com>
2014-11-07 18:30:56 -0600

    Replace dependencies for develop

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,28 +1,3 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-    jasmine_node: {
-      all: ['spec'],
-      options: {
-        coffee: true,
-        useHelpers: true,
-        includeStackTrace: false
-      }
-    },
-    eslint: {
-      all: ['lib/**/*.js']
-    },
-    watch: {
-      all: {
-        files: ['lib/**/*.js', 'spec/*.coffee', 'spec/fixtures/*.*'],
-        tasks: ['eslint', 'jasmine_node']
-      }
-    }
-  });
-
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-jasmine-node');
-  grunt.loadNpmTasks('grunt-eslint');
-
-  grunt.registerTask('test', ['eslint', 'jasmine_node']);
-  grunt.registerTask('default', ['test']);
+  grunt.loadNpmTasks('grunt-parts');
 };
